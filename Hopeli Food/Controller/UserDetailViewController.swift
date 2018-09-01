2e39f1f87f98d59125901ea025af5f8f494410bb
Paul Hofer <paulhofer@pauls-mbp.home>
2018-09-01 23:01:51 +0200

    Apply Basic Setup

---
+++ Hopeli Food/Controller/UserDetailViewController.swift
@@ -0,0 +1,103 @@
+//
+//  UserDetailViewController.swift
+//  Hopeli Food
+//
+//  Created by Paul Hofer on 01.09.18.
+//  Copyright © 2018 Hopeli. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class UserDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let realm = try! Realm()
+    var userDishes: List<Dishes>?
+    
+    @IBOutlet weak var dishesTableView: UITableView!
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
+    var selectedUser: User? {
+        didSet {
+            loadDishesData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userImage.image = UIImage(data: selectedUser?.userImage! as! Data)
+        print("\(userDishes?.count)")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        title = selectedUser?.userName
+        loadDishesData()
+        dishesTableView.reloadData()
+    }
+    
+   
+
+    //MARK: - Data Mathods
+    func loadDishesData () {
+        userDishes = selectedUser?.dishes
+      
+    }
+    
+    
+    //MARK: - Table View View Data Source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userDishes?.count ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = dishesTableView.dequeueReusableCell(withIdentifier: "dishesCell", for: indexPath)
+        
+        if let dish = userDishes?[indexPath.row] {
+            cell.textLabel?.text = dish.dishName
+        }
+        
+        return cell
+        
+        
+    }
+    
+    //    MARK: - going to Detail Dishes Overview
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "goToDishesDetail", sender: self)
+        
+        dishesTableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+    //    MARK: - Add a new Dish
+    @IBAction func addDishButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToDishes", sender: self)
+        
+    }
+    
+    //    MARK: - Prepare for Segue Method
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let identifier = segue.identifier
+        
+        if identifier == "goToDishes" {
+        let destinationVC = segue.destination as! DishesTableViewController
+            destinationVC.selectedUser = selectedUser
+        
+        } else if identifier == "goToDishesDetail" {
+            let destinationVC = segue.destination as! DishesDetailViewController
+           
+            if  let indexPath = dishesTableView.indexPathForSelectedRow {
+                destinationVC.selectedDish = userDishes?[indexPath.row]
+            }
+        }
+    }
+    
+
+}
+
+
